20ef63efe8dea3e77fb31c521da7dec33398e46d
Vladimir <falconv7k@gmail.com>
2021-05-10 11:52:28 +0300

    fix

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -3,7 +3,7 @@ import Movies from '../components/Movies';
 import Search from '../components/Search';
 import Preloader from '../components/Preloader';
 
-const API_KEY = process.env.REACT_APP_API_KEY; 
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default class Main extends Component {
    state = {
@@ -12,24 +12,28 @@ export default class Main extends Component {
    };
 
    componentDidMount() {
-      fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
+      fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
          .then((res) => res.json())
-         .then((data) =>
-            this.setState({ movies: data.Search, loading: false })
-         );
+         .then((data) => this.setState({ movies: data.Search, loading: false }))
+         .catch((err) => {
+            console.error(err);
+            this.setState({ loading: false });
+         });
    }
 
    searchMovies = (text, type = 'all') => {
       this.setState({ loading: true });
       fetch(
-         `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${text}${
+         `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${text}${
             type === 'all' ? '' : `&type=${type}`
          }`
       )
          .then((res) => res.json())
-         .then((data) =>
-            this.setState({ movies: data.Search, loading: false })
-         );
+         .then((data) => this.setState({ movies: data.Search, loading: false }))
+         .catch((err) => {
+            console.error(err);
+            this.setState({ loading: false });
+         });
 
       console.log(this.state);
    };
